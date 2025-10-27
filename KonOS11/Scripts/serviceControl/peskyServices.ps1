1716e356a903c28fcd7841d8d5eb0d91bee219ef
ki8y <wyattdavies22@gmail.com>
2025-10-26 23:48:34 -0400

    chatgpt plz save me

---
+++ KonOS11/Scripts/serviceControl/peskyServices.ps1
@@ -0,0 +1,41 @@
+# Had to get chatgpt to help me with this one im ngl... -_-
+{
+    $services = @(
+        'PimIndexMaintenanceSvc',
+        'WpnUserService',
+        'MessagingService',
+        'cbdhsvc',
+        'CDPUserSvc',
+        'BluetoothUserService',
+        'CaptureService',
+        'ConsentUxUserSvc',
+        'CredentialEnrollmentManagerUserSvc',
+        'DeviceAssociationBrokerSvc',
+        'DevicePickerUserSvc',
+        'P9RdrService',
+        'PenService',
+        'PrintWorkflowUserSvc',
+        'UdkUserSvc',
+        'webthreatdefusersvc',
+        'UnistoreSvc',
+        'UserDataSvc',
+        'NPSMSvc',
+        'DevicesFlowUserSvc',
+        'OneSyncSvc'
+    )
+
+    foreach ($base in $services) {
+        
+        $pattern = '^' + [regex]::Escape($base) + '(_[0-9a-f]+)?$'
+        $matches = Get-Service | Where-Object { $_.Name -match $pattern }
+
+        foreach ($svc in $matches) {
+            try {
+                Set-Service -Name $svc.Name -StartupType Disabled -ErrorAction Stop
+                Write-Host ('🛈 Disabled ' + $svc.Name)
+            } catch {
+                Write-Host ('❎ Could not disable ' + $svc.Name)
+            }
+        }
+    }
+}
