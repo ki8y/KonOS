6c760ae4439cd1794697c6d3003afa5f7204441f
ki8y <wyattdavies22@gmail.com>
2025-11-16 23:23:53 -0500

    Update Bootstrapper.bat

--- KonOS11/Bootstrapper.bat
+++ KonOS11/Bootstrapper.bat
@@ -1,6 +1,6 @@
 @echo off
 :: Download code here
-
 :: Change this to C:\Kon OS\Initialization later on.
+pwsh -Command "Unblock-File -Path '%~dp0KonOS.ps1'; Unblock-File -Path '%~f0'"
 pwsh -ExecutionPolicy Unrestricted -NoProfile -File "%~dp0KonOS.ps1"
 rmdir /s /q "%systemDrive%\Kon OS\KonOS.ps1"
