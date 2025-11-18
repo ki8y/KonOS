bd2e1b5394b2249e5bb6979f652b7bb16068d3a3
ki8y <wyattdavies22@gmail.com>
2025-11-17 23:25:02 -0500

    oops

--- KonOS11/CheckForDependencies.bat
+++ KonOS11/CheckForDependencies.bat
@@ -1,9 +1,9 @@
 @echo off
-mkdir "%systemdrive\Kon OS\Setup\Dependencies" >nul 2>&1
+mkdir "%systemdrive\Kon OS\Dependencies" >nul 2>&1
 fltmc >nul 2>&1
 if not %errorlevel% == 0 (
-    curl -s -L "https://raw.githubusercontent.com/ki8y/KonOS/experimental/KonOS11/CheckForDependencies.ps1" -o "%systemDrive%\Kon OS\Setup\Dependencies\CheckForDependencies.ps1"
     PowerShell -Command "Start-Process -FilePath '%~f0' -Verb RunAs" >nul
     exit
 )
-PowerShell -ExecutionPolicy Bypass -NoProfile -File "%systemDrive\Kon OS\Setup\Dependencies\CheckForDependencies.ps1"
+curl -s -L "https://raw.githubusercontent.com/ki8y/KonOS/experimental/KonOS11/CheckForDependencies.ps1" -o "%systemDrive%\Kon OS\Setup\Dependencies\CheckForDependencies.ps1"
+PowerShell -ExecutionPolicy Bypass -NoProfile -File "%systemDrive\Kon OS\Dependencies\CheckForDependencies.ps1"
