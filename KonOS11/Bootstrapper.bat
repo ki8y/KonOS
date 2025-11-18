1a37d7c74fe30ff53d1d13ba0dcf670d4c13a7df
ki8y <wyattdavies22@gmail.com>
2025-11-17 23:09:03 -0500

    wait not done yet

--- KonOS11/Bootstrapper.bat
+++ KonOS11/Bootstrapper.bat
@@ -1,6 +1,16 @@
 @echo off
 :: Download code here
 :: Change this to C:\Kon OS\Initialization later on.
+
+if exist "%systemdrive\Kon OS\Setup\Dependencies" (
+  goto startscript
+  pause
+) else (
+  curl %putdependenciesfilehere%
+  call "%~dp0Install Dependencies.bat"
+)
+
+:startscript
 pwsh -Command "Unblock-File -Path '%~dp0KonOS.ps1'; Unblock-File -Path '%~f0'"
 pwsh -ExecutionPolicy Unrestricted -NoProfile -File "%~dp0KonOS.ps1"
 rmdir /s /q "%systemDrive%\Kon OS\KonOS.ps1"
