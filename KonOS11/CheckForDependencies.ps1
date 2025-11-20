1a1b9879ebc1f9221167cb75f597d4fcce781b83
ki8y <wyattdavies22@gmail.com>
2025-11-19 23:58:39 -0500

    not working atm

--- KonOS11/CheckForDependencies.ps1
+++ KonOS11/CheckForDependencies.ps1
@@ -1,10 +1,108 @@
 $KonOS='[97m[[38;5;99mKon OS[97m][0m'
+$sound = New-Object System.Media.SoundPlayer
+$sound.SoundLocation = "$env:systemDrive\Windows\Media\Windows Ding.wav"
+
+<# Checks your Windows build to avoid complications. #>
+Write-Host "Checking if you meet the operating system requirements..."
+$Build = [System.Environment]::OSVersion.Version.Build
+
+# Win11 25H2
+if ($build -ge 22600) {
+
+    Write-Host @"
+[33mWindows 11 25H2 Detected! ($Build)[0m
+
+Windows 11 25H2 is an untested version of Windows 11. It should be similar enough to 24H2 to
+run with minimal issues, but I haven't tested this myself. 
+If you encounter any bugs during or after applying these tweaks, please let me know!
+    
+Press any key to continue...
+"@
+    $sound.Play()
+    cmd.exe /c "pause >nul"
+    PromptForDependencies
+}
+
+# Win11 24H2
+if ($build -ge 26100) { PromptForDependencies }
+
+# Win11 23H2
+if ($build -ge 22631) {
+    Write-Host @"
+[31mWindows 11 22H2 Detected! ($Build)[0m
+
+Kon OS was built for Windows 11 24H2. The version of Windows you're using is Windows 11 22H2.
+While Kon OS can be installed on 22H2, I am not responsible for any potential damage caused to your computer.
+Please do not DM me for any help regarding 22H2, I will not respond.
+
+Continue the installation?
+"@
+    $sound.Play()
+    cmd.exe /c "pause >nul"
+    PromptForDependencies
+}
+
+# Win11 22H2
+if ($build -ge 22621) {
+    Write-Host @"
+[31mWindows 11 22H2 Detected! ($Build)[0m
+
+Kon OS was built for Windows 11 24H2. The version of Windows you're using is Windows 11 22H2.
+While Kon OS can be installed on 22H2, I am not responsible for any potential damage caused to your computer.
+Please do not DM me for any help regarding 22H2, I will not respond.
+
+Continue the installation?
+"@
+    $sound.Play()
+    cmd.exe /c "pause >nul"
+    PromptForDependencies
+}
+
+# Win11 21H2
+if ($build -ge 22000) {
+$sound = New-Object System.Media.SoundPlayer
+$sound.SoundLocation = "$env:systemDrive\Windows\Media\Windows Critical Stop.wav"
+
+    Write-Host @"
+[91mSetup Cannot Continue:
+
+[93mWindows 11 21H2 is unsupported by Kon OS.
+Please install a newer version of Windows 11 (ideally 24H2) and try again.
+
+[91m(OUTDATED_WINDOWS_VERSION)
+
+[0mPress any key to exit setup...
+"@
+    $sound.Play()
+    cmd.exe /c "pause >nul"
+    exit
+}
+
+# Windows 10 or lower...
+if ($build -le 19045) {
+$sound = New-Object System.Media.SoundPlayer
+$sound.SoundLocation = "$env:systemDrive\Windows\Media\Windows Critical Stop.wav"
+
+    Write-Host @"
+[91mSetup Cannot Continue:
+
+[93mWindows 10 is unsupported by Kon OS.
+Please install Windows 11 (24H2 or newer) and try again.
+
+[91m(OUTDATED_WINDOWS_VERSION)
+
+[0mPress any key to exit setup...
+"@
+    $sound.Play()
+    cmd.exe /c "pause >nul"
+    exit
+}
 
 function Install-Dependencies {
     Clear-Host
 	
     # Chocolatey
-    $filePath = "$env:systemDrive\ProgramData\chocolatey"
+    $filePath = "$env:systemDrive\ProgramData\chocolatey\bin\choco.exe"
     if (Test-Path -Path $filePath -PathType Leaf) {
         Write-Host "$KonOS Chocolatey is already installed. Skipping..."
     } else {
@@ -22,9 +120,8 @@ function Install-Dependencies {
         Write-Host "$KonOS Installing PowerShell-Core..." -NoNewLine
 		choco install powershell-core --confirm | Out-Null
     }
-	
-	<# VC Redist
-	Couldn't be bothered to check for root directory :/ #>	                
+
+    # .Net Framework 4.8
     Write-Host "$KonOS Installing VCRedist 2015-2022 Runtimes..." -NoNewLine
 	choco install vcredist140 --confirm | Out-Null
 }
@@ -32,17 +129,18 @@ function Install-Dependencies {
 function SelectedNo {
     Clear-Host
     Write-Host "[91mSetup Cannot Continue:`n`n[93mKon OS cannot be installed without the required dependencies.`nPlease install the dependencies and try again.`n`n[91m(MISSING DEPENDENCIES)"
-    Write-Host "`Press any key to exit setup..." -ForegroundColor White -NoNewLine
+    Write-Host "Press any key to exit setup..." -ForegroundColor White -NoNewLine
     cmd.exe /c "pause >nul"
     exit
 }
 
-Write-Host "$KonOS Your computer is missing the files/applications that Kon OS depends on to run.`n`nInstall them now?`n[92m[Y]es [91m[N]o" -NoNewLine
-choice /c YN /n | Out-Null
-switch ($LASTEXITCODE) {
-    1 { Install-Dependencies }
-    2 { SelectedNo }
-}
+function PromptForDependencies {
+    Write-Host "$KonOS Your computer is missing the files/applications that Kon OS depends on to run.`n`nInstall them now?`n[92m[Y]es [91m[N]o" -NoNewLine
+    choice /c YN /n | Out-Null
+    switch ($LASTEXITCODE) {
+        1 { Install-Dependencies }
+        2 { SelectedNo }
+} }
 
 Clear-Host
 Write-Host "$KonOS Successfully installed dependencies!" -ForegroundColor Green
