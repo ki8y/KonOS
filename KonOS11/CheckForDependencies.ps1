564219607eaf4a7e2a09804b31f8610539895d15
ki8y <wyattdavies22@gmail.com>
2025-11-19 22:34:42 -0500

    Update CheckForDependencies.ps1

--- KonOS11/CheckForDependencies.ps1
+++ KonOS11/CheckForDependencies.ps1
@@ -2,32 +2,31 @@ $KonOS='[97m[[38;5;99mKon OS[97m][0m'
 
 function Install-Dependencies {
     Clear-Host
-    $filePath = "$env:systemDrive\Program Files\PowerShell\7\pwsh.exe"
+	
+    # Chocolatey
+    $filePath = "$env:systemDrive\ProgramData\chocolatey"
     if (Test-Path -Path $filePath -PathType Leaf) {
-        Write-Host "$KonOS Powershell 7 is installed. Skipping..."
-	    cmd /c "pause >nul"
+        Write-Host "$KonOS Chocolatey is already installed. Skipping..."
     } else {
-        Write-Host "$KonOS Powershell 7 is not installed. Installing it now..." -NoNewLine
-	    New-Item -Path "$env:SystemDrive\Kon OS\Setup\pwsh" -ItemType Directory -ErrorAction SilentlyContinue | Out-Null
-        cmd /c 'curl -s -L "https://github.com/PowerShell/PowerShell/releases/download/v7.5.4/PowerShell-7.5.4-win-x64.msi" -o "%systemDrive%\Kon OS\Setup\pwsh\PowerShell-7.5.4.msi"' | Out-Null
-        cmd /c 'msiexec /i "%systemDrive%\Kon OS\Setup\pwsh\PowerShell-7.5.4.msi" /q USE_MU=0 ENABLE_MU=0 ALLUSERS=1 /norestart' | Out-Null
-        reg.exe add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" /v "POWERSHELL_TELEMETRY_OPTOUT" /t REG_SZ /d "1" /f | Out-Null
-        reg.exe add "HKEY_CLASSES_ROOT\SystemFileAssociations\.ps1\Shell\Windows.pwsh.Run" /v "MUIVerb" /t REG_SZ /d "Run With Powershell 7" /f | Out-Null
-		reg.exe add "HKEY_CLASSES_ROOT\SystemFileAssociations\.ps1\Shell\Windows.pwsh.Run\Command" | Out-Null
-        New-ItemProperty -Path 'Registry::HKEY_CLASSES_ROOT\SystemFileAssociations\.ps1\Shell\Windows.pwsh.Run\Command' -Name '(Default)' -Value '"C:\Program Files\PowerShell\7\pwsh.exe" -NoExit -NoProfile -ExecutionPolicy Bypass -Command "$host.UI.RawUI.WindowTitle = ''PowerShell 7 (x64)''; & ''%1''"' -Force | Out-Null
-        Write-Host "`r$KonOS Powershell 7 installed successfully.`nPress any key to continue..."
-    cmd /c "pause >nul"
+        Write-Host "$KonOS Installing Chocolatey..." -NoNewLine
+			Set-ExecutionPolicy Bypass -Scope Process -Force | Out-Null
+    		[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072 | Out-Null
+    		Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1')) | Out-Null
+	}
 
-    Clear-Host
-    Write-Host "$KonOS Successfully installed dependencies!" -ForegroundColor Green
-    Write-Host "`nPress any key to launch Kon OS..."
-    cmd /c "pause >nul"
-    Start-Process -FilePath "pwsh.exe" -ArgumentList @(
-    "-NoProfile"
-    "-File"
-    "`"C:\Kon OS\KonOS.ps1`""
-)
-} }
+    # Powershell 7
+	$filePath = "$env:systemDrive\Program Files\PowerShell\7\pwsh.exe"
+    if (Test-Path -Path $filePath -PathType Leaf) {
+        Write-Host "$KonOS PowerShell 7 is already installed. Skipping..."
+    } else {
+        Write-Host "$KonOS Installing PowerShell-Core..." -NoNewLine
+		choco install powershell-core --confirm | Out-Null
+    }
+
+    # .Net Framework 4.8
+    Write-Host "$KonOS Installing VCRedist 2015-2022 Runtimes..." -NoNewLine
+	choco install vcredist140 --confirm | Out-Null
+}
 
 function SelectedNo {
     Clear-Host
@@ -44,3 +43,12 @@ switch ($LASTEXITCODE) {
     2 { SelectedNo }
 }
 
+Clear-Host
+Write-Host "$KonOS Successfully installed dependencies!" -ForegroundColor Green
+Write-Host "`nPress any key to launch Kon OS..."
+cmd /c "pause >nul"
+Start-Process -FilePath "pwsh.exe" -ArgumentList @(
+    "-NoProfile"
+    "-File"
+    "`"C:\Kon OS\KonOS.ps1`""
+)
