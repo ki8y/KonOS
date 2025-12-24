77dd2f4d9553dd15094ef66a66ec431472b76658
ki8y <wyattdavies22@gmail.com>
2025-12-24 02:13:32 -0500

    I forgot wut i did ngl

--- Components/Scripts/generalTweaks.ps1
+++ Components/Scripts/generalTweaks.ps1
@@ -13,14 +13,15 @@ New-ItemProperty -Path "Registry::HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion
 New-ItemProperty -Path "Registry::HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\DriverSearching" -Name "SearchOrderConfig" -PropertyType DWord -Value 0 -Force -WhatIf | Out-Null
 New-ItemProperty -Path "Registry::HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\DriverSearching" -Name "DontSearchWindowsUpdate" -PropertyType DWord -Value 1 -Force -WhatIf | Out-Null
 
-Write-Host "🛈 Disabling automatic driver updates..." -ForegroundColor Blue
+Write-Host "🛈 Disabling all visual effects and animations..."
 New-ItemProperty -Path "Registry::HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\VisualEffects" -Name "VisualFXSetting" -PropertyType DWord -Value 3 -Force -WhatIf | Out-Null 
 New-ItemProperty -Path "Registry::HKCU\Software\Microsoft\Windows\CurrentVersion\Themes\Desktop" -Name "VisualFXSetting" -PropertyType DWord -Value 3 -Force -WhatIf | Out-Null 
 
-Write-Host "🛈 Disabling automatic driver updates..." -ForegroundColor Blue
+Write-Host "🛈 Disabling window animations..." -ForegroundColor Blue
 New-ItemProperty -Path "Registry::HKCU\Control Panel\Desktop\WindowMetrics" -Name "MinAnimate" -PropertyType String -Value 0 -Force -WhatIf | Out-Null
 New-ItemProperty -Path "Registry::HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\VisualEffects" -Name "WindowAnimations" -PropertyType DWord -Value 0 -Force -WhatIf | Out-Null
 New-ItemProperty -Path "Registry::HKCU\Control Panel\Desktop" -Name "MinAnimate" -PropertyType String -Value 0 -Force -WhatIf | Out-Null 
 New-ItemProperty -Path "Registry::HKCU\Software\Microsoft\Windows\DWM" -Name "EnableWindowShadows" -PropertyType DWord -Value 0 -Force -WhatIf | Out-Null 
 New-ItemProperty -Path "Registry::HKCU\Control Panel\Desktop" -Name "DragFullWindows" -PropertyType String -Value 0 -Force -WhatIf | Out-Null 
-pause
+
+Write-Host "🛈 Disabling Taskbar animations..."
