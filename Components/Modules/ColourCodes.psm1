60050e83b090a800a225a788cc5a0f11c8f25a9d
ki8y <wyattdavies22@gmail.com>
2025-12-29 21:35:12 -0500

    Colour COde thing  yep

---
+++ Components/Modules/ColourCodes.psm1
@@ -0,0 +1,23 @@
+# ESC colour codes. Helpful for powershell 5.1 and the message function in the throbber thingy yeh something like that
+
+# Dark colours
+$Black = '[30m'
+$DarkRed = '[31m'
+$DarkGreen = '[32m'
+$DarkYellow = '[33m'
+$DarkBlue = '[34m'
+$DarkMagenta = '[35m'
+$DarkCyan = '[36m'
+$DarkWhite = '[37m' # as far as i know this is literally useless lol just use $White instead
+
+# Regular Colours
+$Gray = '[90m'
+$Red = '[91m'
+$Green = '[92m'
+$Yellow = '[93m'
+$Blue = '[94m'
+$Magenta = '[95m'
+$Cyan = '[96m'
+$White = '[97m'
+
+$Reset = '[39m' # Would rarely use this, can cause issues in powershell 5.1 with the default blue window.
