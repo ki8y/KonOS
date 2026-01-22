d26fc1a3c192a02a02f25e24240019c141a8e394
Kon <wyattdavies22@gmail.com>
2026-01-21 19:06:14 -0500

    Delete KonOS11 directory

--- KonOS11/CheckForDependencies.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-mkdir "%systemdrive%\Kon OS\Dependencies" >nul 2>&1
-fltmc >nul 2>&1
-if not %errorlevel% == 0 (
-    PowerShell -Command "Start-Process -FilePath '%~f0' -Verb RunAs" >nul
-    exit
-)
-curl -s -L "https://raw.githubusercontent.com/ki8y/KonOS/experimental/KonOS11/CheckForDependencies.ps1" -o "%systemDrive%\Kon OS\Dependencies\CheckForDependencies.ps1"
-PowerShell -ExecutionPolicy Unrestricted -NoProfile -File "%systemDrive%\Kon OS\Dependencies\CheckForDependencies.ps1"
