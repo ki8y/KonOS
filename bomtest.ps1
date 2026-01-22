c3f0fe749bfa0989541cbd1601b2ad45689bb969
ki8y <wyattdavies22@gmail.com>
2026-01-21 22:29:32 -0500

    Update bomtest.ps1

--- bomtest.ps1
+++ bomtest.ps1
@@ -1,4 +1,4 @@
-﻿# Yes
+﻿
 Start-Sleep -Milliseconds 15
 $Host.UI.RawUI.WindowTitle = "Kon OS UTF-8 PowerShell 5.1 Test"
 $host.UI.RawUI.WindowSize = New-Object System.Management.Automation.Host.Size(120,30)
@@ -164,7 +164,8 @@ cmd /c "pause >nul"
 
 function Exit-Test {
     Write-Host "Exiting Kon OS UTF-8 test... cya later :)"
-    Start-Sleep 3
+    Start-Sleep 1
+    [System.Environment]::Exit(0)
 }
 
 Write-Host @"
