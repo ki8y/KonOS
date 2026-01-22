cb31deadfa0ba943c4148883fe61cefcb4fabc3c
ki8y <wyattdavies22@gmail.com>
2026-01-22 05:41:26 -0500

    Fixed Global Colours
    Changed all of the variables to be global variables so that they can be used across any script. (This was the original intention, and it used to work without it. I'm not sure why it stopped working but this seems to have fixed it)

--- Components/Modules/ColourCodes.psm1
+++ Components/Modules/ColourCodes.psm1
@@ -1,50 +1,50 @@
 # ESC colour codes. Helpful for powershell 5.1 and the message function in the throbber thingy yeh something like that
 
 # Dark colours
-$Black = '[30m'
-$DarkRed = '[31m'
-$DarkGreen = '[32m'
-$DarkYellow = '[33m'
-$DarkBlue = '[34m'
-$DarkMagenta = '[35m'
-$DarkCyan = '[36m'
-$DarkWhite = '[37m' # as far as i know this is literally useless lol just use $White instead
+$global:Black = '[30m'
+$global:DarkRed = '[31m'
+$global:DarkGreen = '[32m'
+$global:DarkYellow = '[33m'
+$global:DarkBlue = '[34m'
+$global:DarkMagenta = '[35m'
+$global:DarkCyan = '[36m'
+$global:DarkWhite = '[37m' # as far as i know this is literally useless lol just use $global:White instead
 
 # Regular Colours
-$Gray = '[90m'
-$Red = '[91m'
-$Green = '[92m'
-$Yellow = '[93m'
-$Blue = '[94m'
-$Magenta = '[95m'
-$Cyan = '[96m'
-$White = '[97m'
+$global:Gray = '[90m'
+$global:Red = '[91m'
+$global:Green = '[92m'
+$global:Yellow = '[93m'
+$global:Blue = '[94m'
+$global:Magenta = '[95m'
+$global:Cyan = '[96m'
+$global:White = '[97m'
 
 # Dark colours (Background)
-$BlackBG = '[40m'
-$DarkRedBG = '[41m'
-$DarkGreenBG = '[42m'
-$DarkYellowBG = '[43m'
-$DarkBlueBG = '[44m'
-$DarkMagentaBG = '[45m'
-$DarkCyanBG = '[46m'
-$DarkWhiteBG = '[47m' # Still useless
+$global:BlackBG = '[40m'
+$global:DarkRedBG = '[41m'
+$global:DarkGreenBG = '[42m'
+$global:DarkYellowBG = '[43m'
+$global:DarkBlueBG = '[44m'
+$global:DarkMagentaBG = '[45m'
+$global:DarkCyanBG = '[46m'
+$global:DarkWhiteBG = '[47m' # Still useless
 
 # Regular Colours (Background)
-$GrayBG = '[100m'
-$RedBG = '[101m'
-$GreenBG = '[102m'
-$YellowBG = '[103m'
-$BlueBG = '[104m'
-$MagentaBG = '[105m'
-$CyanBG = '[106m'
-$WhiteBG = '[107m'
+$global:GrayBG = '[100m'
+$global:RedBG = '[101m'
+$global:GreenBG = '[102m'
+$global:YellowBG = '[103m'
+$global:BlueBG = '[104m'
+$global:MagentaBG = '[105m'
+$global:CyanBG = '[106m'
+$global:WhiteBG = '[107m'
 
-$Reset = '[39m' # Would rarely use this, can cause issues in powershell 5.1 with the default blue window.
-$ResetBG = '[49m' # Ditto
+$global:Reset = '[39m' # Would rarely use this, can cause issues in powershell 5.1 with the default blue window.
+$global:ResetBG = '[49m' # Ditto
 
 # Kon OS purple :D
-$accent = '[38;5;99m'
-$accentBG = '[48;5;99m'
+$global:accent = '[38;5;99m'
+$global:accentBG = '[48;5;99m'
 
-$ESC = '' # Helpful for any other esc code needed
+$global:ESC = '' # Helpful for any other esc code needed
