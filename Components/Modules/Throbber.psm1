092e02c003da33e37511cde0a49f36dc6c8c9b61
ki8y <wyattdavies22@gmail.com>
2026-01-22 05:42:32 -0500

    Added Colour Parameter
    I made it easier for myself to choose a colour I want the loading spinner to be. :)

--- Components/Modules/Throbber.psm1
+++ Components/Modules/Throbber.psm1
@@ -1,16 +1,18 @@
+﻿Import-Module "$env:systemDrive\Kon OS\Modules\ColourCodes.psm1"
 function Show-Throbber {
     param(
+        [string]$Colour,    
         [string]$Message,
         [scriptblock]$Action
     )
 
-    $spinnyThing = @('\','|','/','|')
+    $spinnyThing = @('╭','|','╭','|')
     $i = 0
 
     $run = Start-Job -ScriptBlock $Action
 
     while ($run.State -eq 'Running') {
-        Write-Host -NoNewline "`r[$($spinnyThing[$i])] $Message[?25l"
+        Write-Host -NoNewline "`r$($Colour)[$($spinnyThing[$i])] $($Message)[?25l"
         Start-Sleep -Milliseconds 100
         $i = ($i + 1) % $spinnyThing.Length
     }
