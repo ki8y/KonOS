8fa7a155f82222c5a46f666a04efe8d7c3a3cfd8
Kon <wyattdavies22@gmail.com>
2026-01-29 05:06:22 -0500

    test

--- funnytestscript.ps1
+++ funnytestscript.ps1
@@ -1,14 +1,15 @@
 $env:PATH = "$PSScriptRoot\Components\Modules\PsExec;$env:PATH"
-Clear-Host
+#Clear-Host
+
+# parent script
 Write-Host "Parent script"
-Write-Host "Is Admin:" -NoNewLine
-([Security.Principal.WindowsIdentity]::GetCurrent()).Groups -contains 'S-1-5-32-544' | Write-Host;
-PsExec64 -accepteula -l -nobanner `
-wt.exe --fullscreen sp `; @"
-Write-Host `"hai`";
-Write-Host `"Is Admin:`" -NoNewLine;
-([Security.Principal.WindowsIdentity]::GetCurrent()).Groups -contains 'S-1-5-32-544' | Write-Host
-"@
+Write-Host "Is Admin:" -NoNewLine; ([Security.Principal.WindowsIdentity]::GetCurrent()).Groups -contains 'S-1-5-32-544' | Write-Host
+
+# child script
+PsExec64 -accepteula -l -nobanner wt.exe --fullscreen Powershell -Command "Invoke-Expression ((Invoke-RestMethod https://raw.githubusercontent.com/ki8y/KonOS/master/Bootstrapper.ps1).TrimStart([char]0xFEFF))"
+
 pause
 
 # -NoProfile -ExecutionPolicy Bypass -Command 
+Write-Host `"Is Admin:`" -NoNewLine
+([Security.Principal.WindowsIdentity]::GetCurrent()).Groups -contains 'S-1-5-32-544' | Write-Host
