4614ffe9db88cc70bce7f4cbfe39f5fb553ead33
Kon <wyattdavies22@gmail.com>
2026-03-26 21:49:05 -0400

    Fixed variable thing
    Oops

--- Components/Setup/Modules/Invoke-SpeedRequest.psm1
+++ Components/Setup/Modules/Invoke-SpeedRequest.psm1
@@ -1,16 +1,14 @@
-$SpeedRequest = {
-    function Invoke-SpeedRequest { 
-        param(
-            [string]$uri,    
-            [string]$outFile,
-            [switch]$Silent
-        )
+function Invoke-SpeedRequest { 
+    param(
+        [string]$uri,    
+        [string]$outFile,
+        [switch]$Silent
+    )
 
-        if ($Silent) {
-            curl.exe -s -L "$uri" -o "$OutFile"
-        }
-        elseif (-not $Silent) {
-            curl.exe -L "$uri" -o "$OutFile"
-        }
+    if ($Silent) {
+        curl.exe -s -L "$uri" -o "$OutFile"
+    }
+    elseif (-not $Silent) {
+        curl.exe -L "$uri" -o "$OutFile"
     }
 }
