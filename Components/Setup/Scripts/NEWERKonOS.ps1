b1e61bfa0529116f3fa9526eaac068cf65609c84
Kon <wyattdavies22@gmail.com>
2026-04-02 00:04:40 -0400

    Overhaul
    - Choices are fixed
    - Made it look pretty
    - Added a feature where you need to confirm to not use a restore point
    - Added a feature where if u missclick u can go back and redo your
    settings

--- Components/Setup/Scripts/NEWERKonOS.ps1
+++ Components/Setup/Scripts/NEWERKonOS.ps1
@@ -1,24 +1,45 @@
-if ($env:WT_SESSION) { $WTSession = $true }
-else { $WTSession = $false }
+﻿<# KonOS.ps1
+Built for Windows PowerShell 5.1 & PowerShell 7.6 (LTS)
 
-# Choices
-Write-Host "Create a restore point? (Y/N) *Recommended*"
-Read-Choice -Name "CreateRP"
+This is basically just bootstrapper.ps1 but I split it up so it's easier for me to read. #>
 
-Write-Host "Uninstall Microsoft Edge? (Y/N)"
-Read-Choice -Name "RemoveEdge"
+# choice prompts for setup so u can go afk
 
-Write-Host "Would you like to uninstall Microsoft Store? (Y/N)"
-Read-Choice -Name "RemoveWS"
+$esc = ([char]27)
 
-Write-Host "Disable Wi-Fi? (Y/N)"
-Read-Choice -Name "DisableWifi"
+Import-Module
 
-Write-Host "Disable Wi-Fi? (Y/N)"
-Read-Choice -Name "DisableWifi"
+do {
+    Clear-Host
+    if ($env:WT_SESSION) { $WTSession = $true } else { $WTSession = $false }
+
+    $KONOS = "$env:systemDrive\Kon OS"
+    Import-Module "$HOME\Documents\GitHub\KonOS\Components\Setup\Modules\Read-Choice.psm1"
+    # Import-Module "$KonOS\Setup\Modules\Read-Choice.psm1" (FINAL FOR SCRIPT)
+
+    # Choices
+    Write-Host "Create a restore point? [Y/N] $($ESC)[33m*HIGHLY Recommended*$($ESC)[97m"
+    $CreateRP = Read-Choice
+
+    if (-not $CreateRP) {
+        Write-Host "`nAre you sure!? Not creating a restore point can complicate things if things go seriously wrong.`nPlease type `"I understand the risks`" to proceed without a restore point. Otherwise, press enter to continue.`n$($ESC)[94m» " -ForegroundColor Red -NoNewLine
+        $ConfirmNoRP = Read-Host
+        if ($ConfirmNoRP -notmatch "I understand the risks") {
+            $CreateRP = $true
+        }
+    }
+
+    Write-Host "`nUninstall Microsoft Edge? (You'll get to choose a browser) [Y/N]"
+    $RemoveEdge = Read-Choice
+
+    Write-Host "`nWould you like to uninstall Microsoft Store? [Y/N]"
+    $RemoveWS = Read-Choice
+
+    Write-Host "`nDisable Wi-Fi? [Y/N]"
+    $DisableWifi = Read-Choice
 
 $setup = [PSCustomObject]@{
-    "Flags" = @{
+    "Flags" = [PSCustomObject]@{
         "WTSession" = $WTSession
     }
     "Prefs" = [PSCustomObject]@{
@@ -29,4 +50,23 @@ $setup = [PSCustomObject]@{
     }
 }
 
-$setup | ConvertTo-Json | Set-Content -Path "$KONOS\Setup\setupConfig.json"
+    Clear-Host
+Write-Host @"
+├────── These are your settings ──────┤
+
+Create restore point    =  [$($CreateRP)]
+Remove Microsoft Edge   =  [$($RemoveEdge)]
+Remove Microsoft Store  =  [$($RemoveWS)]
+Disable Wi-Fi           =  [$($DisableWifi)]
+"@
+    
+    Write-Host "Is this okay? [Y/N]"
+    $ConfirmPrefs = Read-Choice
+
+} while (-not $ConfirmPrefs)
+
+New-Item -ItemType File -Path "$KonOS\Setup\setupConfig.json" -Force -ErrorAction Stop | Out-Null
+$setup | ConvertTo-Json | Set-Content -Path "$KONOS\Setup\setupConfig.json" -Encoding UTF8 # this does some pretty funny stuff on powershell 5.1. still works though so idc 🤷‍♂️
+
+<#Write-Output "[Debug] Opening config file in text editor..."
+start-process "notepad++" -ArgumentList "`"C:\Kon OS\Setup\setupConfig.json`""#>
