a9f4ec5a643d0ce49fa76567864cde0ec3f03183
Kon <wyattdavies22@gmail.com>
2026-04-04 03:08:29 -0400

    Added express installations
    - Exactly what the summary says, but yea.
    - Temporary enabled debug thingy again

--- Components/Setup/Scripts/NEWERKonOS.ps1
+++ Components/Setup/Scripts/NEWERKonOS.ps1
@@ -12,6 +12,7 @@ $KONOS = "$env:systemDrive\Kon OS"
 
 Clear-Host
 if ($env:WT_SESSION) { $WTSession = $true } else { $WTSession = $false }
+
 Write-Host "How would you like to setup Kon OS?"
 Write-Host @"
 [1] Express installation - Automatically apply recommended setup settings
@@ -19,49 +20,49 @@ Write-Host @"
 "@
 choice.exe /c 12 /n | Out-Null
 switch ($LASTEXITCODE) {
-    1 { <#express installation#> }
-    2 { <#Custom installation#> }
+    1 { $ExpressInstall = $True }
+    2 { $ExpressInstall = $False }
 }
-# ^^^^ this could probably be done with the confirmprefs variable but im in class so i gotta wrap this up for now LOL
+Clear-Host
 
-do {
+if (-not $ExpressInstall) {
 
-    # Import-Module "$KonOS\Setup\Modules\Read-Choice.psm1" (FINAL FOR SCRIPT)
+    do {
 
-    # Choices
-    Write-Host "Create a restore point? [Y/N] $($ESC)[33m*HIGHLY Recommended*$($ESC)[97m"
-    $CreateRP = Read-Choice
+        # Choices
+        Write-Host "Create a restore point? [Y/N] $($ESC)[33m*HIGHLY Recommended*$($ESC)[97m"
+        $CreateRP = Read-Choice
 
-    if (-not $CreateRP) {
-        Write-Host "`nAre you sure!? Not creating a restore point can complicate things if things go seriously wrong.`nPlease type `"I understand the risks`" to proceed without a restore point. Otherwise, press enter to continue.`n$($ESC)[94m» " -ForegroundColor Red -NoNewLine
-        $ConfirmNoRP = Read-Host
-        if ($ConfirmNoRP -notmatch "I understand the risks") {
-            $CreateRP = $true
+        if (-not $CreateRP) {
+            Write-Host "`nAre you sure!? Not creating a restore point can complicate things if things go seriously wrong.`nPlease type `"I understand the risks`" to proceed without a restore point. Otherwise, press enter to continue.`n$($ESC)[94m» " -ForegroundColor Red -NoNewLine
+            $ConfirmNoRP = Read-Host
+            if ($ConfirmNoRP -notmatch "I understand the risks") {
+                $CreateRP = $true
+            }
         }
-    }
 
-    Write-Host "`nUninstall Microsoft Edge? (You'll get to choose a browser) [Y/N]"
-    $RemoveEdge = Read-Choice
+        Write-Host "`nUninstall Microsoft Edge? (You'll get to choose a browser) [Y/N]"
+        $RemoveEdge = Read-Choice
 
-    Write-Host "`nWould you like to uninstall Microsoft Store? [Y/N]"
-    $RemoveWS = Read-Choice
+        Write-Host "`nWould you like to uninstall Microsoft Store? [Y/N]"
+        $RemoveWS = Read-Choice
 
-    Write-Host "`nDisable Wi-Fi? [Y/N]"
-    $DisableWifi = Read-Choice
+        Write-Host "`nDisable Wi-Fi? [Y/N]"
+        $DisableWifi = Read-Choice
 
-    $setup = [PSCustomObject]@{
-        "Flags" = [PSCustomObject]@{
-            "WTSession" = $WTSession
+        $setup = [PSCustomObject]@{
+            "Flags" = [PSCustomObject]@{
+                "WTSession" = $WTSession
+            }
+            "Prefs" = [PSCustomObject]@{
+                "CreateRP"   = $CreateRP
+                "RemoveEdge" = $RemoveEdge
+                "RemoveWS"   = $RemoveWS
+                "DisableWifi" = $DisableWifi
+            }
         }
-        "Prefs" = [PSCustomObject]@{
-            "CreateRP"   = $CreateRP
-            "RemoveEdge" = $RemoveEdge
-            "RemoveWS"   = $RemoveWS
-            "DisableWifi" = $DisableWifi
-        }
-    }
 
-    Clear-Host
+        Clear-Host
 Write-Host @"
 ├────── These are your settings ──────┤
 
@@ -71,13 +72,27 @@ Remove Microsoft Store  =  [$($RemoveWS)]
 Disable Wi-Fi           =  [$($DisableWifi)]
 "@
     
-    Write-Host "Is this okay? [Y/N]"
-    $ConfirmPrefs = Read-Choice
+        Write-Host "Is this okay? [Y/N]"
+        $ConfirmPrefs = Read-Choice
 
-} while (-not $ConfirmPrefs)
+    } while (-not $ConfirmPrefs)
+}
+else {
+    $setup = [PSCustomObject]@{
+        "Flags" = [PSCustomObject]@{
+            "WTSession" = $WTSession
+        }
+        "Prefs" = [PSCustomObject]@{
+            "CreateRP"   = $True
+            "RemoveEdge" = $True
+            "RemoveWS"   = $True
+            "DisableWifi" = $False
+        }
+    }
+}
 
 New-Item -ItemType File -Path "$KonOS\Setup\setupConfig.json" -Force -ErrorAction Stop | Out-Null
 $setup | ConvertTo-Json | Set-Content -Path "$KONOS\Setup\setupConfig.json" -Encoding UTF8 # this does some pretty funny stuff on powershell 5.1. still works though so idc 🤷‍♂️
 
-<#Write-Output "[Debug] Opening config file in text editor..."
-start-process "notepad++" -ArgumentList "`"C:\Kon OS\Setup\setupConfig.json`""#>
+Write-Output "[Debug] Opening config file in text editor..."
+start-process "notepad++" -ArgumentList "`"C:\Kon OS\Setup\setupConfig.json`""
