2a6ed885fc643bc6bf2f37a82a8a478b85fe6ffa
Kon <wyattdavies22@gmail.com>
2026-04-10 15:44:21 -0400

    Update Write-Box.psm1
    Added:
    
    - New Colour System (uses ESC codes instead of the built in powershell
    colours, so I can add basically whatever I want)
    - Highlight parameter
        - Able to highlight the text within the box white
        - Has no effect if using the "None" border style
    
    Changed:
    - Re-Formatted
    
    P.S: Forgot to add accent as an accepted parameter. Fixed :)

--- Components/Universal/Modules/Write-Box.psm1
+++ Components/Universal/Modules/Write-Box.psm1
@@ -1,32 +1,64 @@
 function Write-Box {
-    [CmdletBinding(DefaultParameterSetName='Indent')]
+    [CmdletBinding(DefaultParameterSetName = 'Indent')]
     param(
       
-      [Parameter(Mandatory=$True)]
-      [string]$Text,
+        [Parameter(Mandatory = $True)]
+        [string]$Text,
       
-      [int]$Padding = 1,
+        [int]$Padding = 1,
      
-      [Parameter(ParameterSetName='Indent')]
-      [int]$Indent = 0,
+        [Parameter(ParameterSetName = 'Indent')]
+        [int]$Indent = 0,
       
-      [Parameter(ParameterSetName='Centered')]
-      [switch]$Centered,
+        [Parameter(ParameterSetName = 'Centered')]
+        [switch]$Centered,
 
-      # Colours
-      [ValidateSet('Black', 'DarkBlue', 'DarkGreen', 'DarkCyan', 'DarkRed', 'DarkMagenta', 'DarkYellow', 'Gray', 'DarkGray', 'Blue', 'Green', 'Cyan', 'Red', 'Magenta', 'Yellow', 'White')]
-      [Alias("Colour")]
-      [string]$Color = 'White',
+        [switch]$HighlightText = $false,
 
-      [string]$Border = 'Sharp'
+        [switch]$NoNewLine = $false,
+
+        # Colours
+        [ValidateSet('Accent', 'Black', 'DarkBlue', 'DarkGreen', 'DarkCyan', 'DarkRed', 'DarkMagenta', 'DarkYellow', 'Gray', 'DarkGray', 'Blue', 'Green', 'Cyan', 'Red', 'Magenta', 'Yellow', 'White', 'Accent')]
+        [Alias("Colour")]
+        [string]$Color = 'White',
+
+        [string]$Border = 'Sharp'
     )
+
+    $esc = ([char]27)
 
     $conWidth = (Get-Host).UI.RawUI.WindowSize.Width
 
+    switch ($Color) {
+        Accent { $fColor = "$($esc)[38;5;99m" }
+        Black { $fColor = "$($esc)[30m" }
+        DarkBlue { $fColor = "$($esc)[34m" }
+        DarkGreen { $fColor = "$($esc)[32m" }
+        DarkCyan { $fColor = "$($esc)[36m" }
+        DarkRed { $fColor = "$($esc)[31m" }
+        DarkMagenta { $fColor = "$($esc)[35m" }
+        DarkYellow { $fColor = "$($esc)[33m" }
+        DarkGray { $fColor = "$($esc)[90m" }
+        Gray { $fColor = "$($esc)[37m" }
+        Blue { $fColor = "$($esc)[94m" }
+        Green { $fColor = "$($esc)[92m" }
+        Cyan { $fColor = "$($esc)[96m" }
+        Red { $fColor = "$($esc)[91m" }
+        Magenta { $fColor = "$($esc)[95m" }
+        Yellow { $fColor = "$($esc)[93m" }
+        White { $fColor = "$($esc)[97m" }
+    }
+
+    switch ($HighlightText) {
+        true { $highlighter = "$($esc)[97m" }
+        false { $highlighter = $fcolor }
+    }
+
     $innerWidth = $Text.Length + ($Padding * 2)
-    if ($centered) { # This gave me a headache
-      $Indent = [Math]::Max(0, [Math]::Floor(($conWidth - ($innerwidth + 2)) / 2))
-      $rawIndent = [Math]::Max(0, [Math]::Floor(($conWidth - ($Text.Length)) / 2))
+    if ($centered) {
+        # This gave me a headache
+        $Indent = [Math]::Max(0, [Math]::Floor(($conWidth - ($innerwidth + 2)) / 2))
+        $rawIndent = [Math]::Max(0, [Math]::Floor(($conWidth - ($Text.Length)) / 2))
     }
 
     $offset = " " * $Indent
@@ -34,25 +66,25 @@ function Write-Box {
 
     switch ($Border) {
         round {
-          Write-Host ("$offset" + "╭" + ("─" * $innerWidth) + "╮") -ForegroundColor $Color
-          Write-Host ("$offset" + "│" + (" " * $Padding) + $Text + (" " * $Padding) + "│") -ForegroundColor $Color
-          Write-Host ("$offset" + "╰" + ("─" * $innerWidth) + "╯") -ForegroundColor $Color
+            Write-Host ("$offset" + "$fColor" + "╭" + ("─" * $innerWidth) + "╮")
+            Write-Host ("$offset" + "$fColor" + "│" + (" " * $Padding) + "$Highlighter" + $Text + "$fColor" + (" " * $Padding) + "│")
+            Write-Host ("$offset" + "$fColor" + "╰" + ("─" * $innerWidth) + "╯") -NoNewline:$NoNewLine
         }
 
         sharp {
-          Write-Host ("$offset" + "┌" + ("─" * $innerWidth) + "┐") -ForegroundColor $Color
-          Write-Host ("$offset" + "│" + (" " * $Padding) + $Text + (" " * $Padding) + "│") -ForegroundColor $Color
-          Write-Host ("$offset" + "└" + ("─" * $innerWidth) + "┘") -ForegroundColor $Color
+            Write-Host ("$offset" + "$fColor" + "┌" + ("─" * $innerWidth) + "┐")
+            Write-Host ("$offset" + "$fColor" + "│" + (" " * $Padding) + "$Highlighter" + $Text + "$fColor" + (" " * $Padding) + "│")
+            Write-Host ("$offset" + "└" + ("─" * $innerWidth) + "┘") -NoNewline:$NoNewLine
         }
 
         double { 
-          Write-Host ("$offset" + "╔" + ("═" * $innerWidth) + "╗") -ForegroundColor $Color
-          Write-Host ("$offset" + "║" + (" " * $Padding) + $Text + (" " * $Padding) + "║") -ForegroundColor $Color
-          Write-Host ("$offset" + "╚" + ("═" * $innerWidth) + "╝") -ForegroundColor $Color
+            Write-Host ("$offset" + "$fColor" + "╔" + ("═" * $innerWidth) + "$fColor" + "╗")
+            Write-Host ("$offset" + "$fColor" + "║" + (" " * $Padding) + "$Highlighter" + $Text + "$fColor" + (" " * $Padding) + "║")
+            Write-Host ("$offset" + "$fColor" + "╚" + ("═" * $innerWidth) + "$fColor" + "╝") -NoNewline:$NoNewLine
         }
 
         none {
-          Write-Host ($RawOffset + $Text) -ForegroundColor $Color
+            Write-Host ($RawOffset + $Text) -NoNewline:$NoNewLine
         }
     }
 }
