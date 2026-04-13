28d7a2435a27e50e0cba046b624fac25629856ab
Kon <199155976+ki8y@users.noreply.github.com>
2026-04-13 01:04:59 -0400

    yes

--- PublicTest/CenteredTitleScreenTest.ps1
+++ PublicTest/CenteredTitleScreenTest.ps1
@@ -9,64 +9,94 @@ $global:White = "$($esc)[97m"
 $accent = "$($esc)[38;5;99m"
 
 function Write-Box {
-    [CmdletBinding(DefaultParameterSetName='Indent')]
-    param(
+	[CmdletBinding(DefaultParameterSetName = 'Indent')]
+	param(
       
-      [Parameter(Mandatory=$True)]
-      [string]$Text,
+		[Parameter(Mandatory = $True)]
+		[string]$Text,
       
-      [int]$Padding = 1,
+		[int]$Padding = 1,
      
-      [Parameter(ParameterSetName='Indent')]
-      [int]$Indent = 0,
+		[Parameter(ParameterSetName = 'Indent')]
+		[int]$Indent = 0,
       
-      [Parameter(ParameterSetName='Centered')]
-      [switch]$Centered,
+		[Parameter(ParameterSetName = 'Centered')]
+		[switch]$Centered,
 
-      [switch]$NoNewLine = $false,
+		[switch]$HighlightText = $false,
 
-      # Colours
-      [ValidateSet('Black', 'DarkBlue', 'DarkGreen', 'DarkCyan', 'DarkRed', 'DarkMagenta', 'DarkYellow', 'Gray', 'DarkGray', 'Blue', 'Green', 'Cyan', 'Red', 'Magenta', 'Yellow', 'White')]
-      [Alias("Colour")]
-      [string]$Color = 'White',
+		[switch]$NoNewLine = $false,
 
-      [string]$Border = 'Sharp'
-    )
+		# Colours
+		[ValidateSet('Black', 'DarkBlue', 'DarkGreen', 'DarkCyan', 'DarkRed', 'DarkMagenta', 'DarkYellow', 'Gray', 'DarkGray', 'Blue', 'Green', 'Cyan', 'Red', 'Magenta', 'Yellow', 'White', 'Accent')]
+		[Alias("Colour")]
+		[string]$Color = 'White',
 
-    $conWidth = (Get-Host).UI.RawUI.WindowSize.Width
+		[string]$Border = 'Sharp'
+	)
 
-    $innerWidth = $Text.Length + ($Padding * 2)
-    if ($centered) { # This gave me a headache
-      $Indent = [Math]::Max(0, [Math]::Floor(($conWidth - ($innerwidth + 2)) / 2))
-      $rawIndent = [Math]::Max(0, [Math]::Floor(($conWidth - ($Text.Length)) / 2))
-    }
+	$esc = ([char]27)
 
-    $offset = " " * $Indent
-    $rawOffset = " " * $RawIndent
+	$conWidth = (Get-Host).UI.RawUI.WindowSize.Width
 
-    switch ($Border) {
-        round {
-          Write-Host ("$offset" + "╭" + ("─" * $innerWidth) + "╮") -ForegroundColor $Color
-          Write-Host ("$offset" + "│" + (" " * $Padding) + $Text + (" " * $Padding) + "│") -ForegroundColor $Color
-          Write-Host ("$offset" + "╰" + ("─" * $innerWidth) + "╯") -ForegroundColor $Color -NoNewLine:$NoNewLine
-        }
+	switch ($Color) {
+		Accent { $fColor = "$($esc)[38;5;99m" }
+		Black { $fColor = "$($esc)[30m" }
+		DarkBlue { $fColor = "$($esc)[34m" }
+		DarkGreen { $fColor = "$($esc)[32m" }
+		DarkCyan { $fColor = "$($esc)[36m" }
+		DarkRed { $fColor = "$($esc)[31m" }
+		DarkMagenta { $fColor = "$($esc)[35m" }
+		DarkYellow { $fColor = "$($esc)[33m" }
+		DarkGray { $fColor = "$($esc)[90m" }
+		Gray { $fColor = "$($esc)[37m" }
+		Blue { $fColor = "$($esc)[94m" }
+		Green { $fColor = "$($esc)[92m" }
+		Cyan { $fColor = "$($esc)[96m" }
+		Red { $fColor = "$($esc)[91m" }
+		Magenta { $fColor = "$($esc)[95m" }
+		Yellow { $fColor = "$($esc)[93m" }
+		White { $fColor = "$($esc)[97m" }
+	}
 
-        sharp {
-          Write-Host ("$offset" + "┌" + ("─" * $innerWidth) + "┐") -ForegroundColor $Color
-          Write-Host ("$offset" + "│" + (" " * $Padding) + $Text + (" " * $Padding) + "│") -ForegroundColor $Color
-          Write-Host ("$offset" + "└" + ("─" * $innerWidth) + "┘") -ForegroundColor $Color -NoNewLine:$NoNewLine
-        }
+	switch ($HighlightText) {
+		true { $highlighter = "$($esc)[97m" }
+		false { $highlighter = $fcolor }
+	}
 
-        double { 
-          Write-Host ("$offset" + "╔" + ("═" * $innerWidth) + "╗") -ForegroundColor $Color
-          Write-Host ("$offset" + "║" + (" " * $Padding) + $Text + (" " * $Padding) + "║") -ForegroundColor $Color
-          Write-Host ("$offset" + "╚" + ("═" * $innerWidth) + "╝") -ForegroundColor $Color -NoNewLine:$NoNewLine
-        }
+	$innerWidth = $Text.Length + ($Padding * 2)
+	if ($centered) {
+		# This gave me a headache
+		$Indent = [Math]::Max(0, [Math]::Floor(($conWidth - ($innerwidth + 2)) / 2))
+		$rawIndent = [Math]::Max(0, [Math]::Floor(($conWidth - ($Text.Length)) / 2))
+	}
 
-        none {
-          Write-Host ($RawOffset + $Text) -ForegroundColor $Color -NoNewLine:$NoNewLine
-        }
-    }
+	$offset = " " * $Indent
+	$rawOffset = " " * $RawIndent
+
+	switch ($Border) {
+		round {
+			Write-Host ("$offset" + "$fColor" + "╭" + ("─" * $innerWidth) + "╮")
+			Write-Host ("$offset" + "$fColor" + "│" + (" " * $Padding) + "$Highlighter" + $Text + "$fColor" + (" " * $Padding) + "│")
+			Write-Host ("$offset" + "$fColor" + "╰" + ("─" * $innerWidth) + "╯") -NoNewline:$NoNewLine
+		}
+
+		sharp {
+			Write-Host ("$offset" + "$fColor" + "┌" + ("─" * $innerWidth) + "┐")
+			Write-Host ("$offset" + "$fColor" + "│" + (" " * $Padding) + "$Highlighter" + $Text + "$fColor" + (" " * $Padding) + "│")
+			Write-Host ("$offset" + "└" + ("─" * $innerWidth) + "┘") -NoNewline:$NoNewLine
+		}
+
+		double { 
+			Write-Host ("$offset" + "$fColor" + "╔" + ("═" * $innerWidth) + "$fColor" + "╗")
+			Write-Host ("$offset" + "$fColor" + "║" + (" " * $Padding) + "$Highlighter" + $Text + "$fColor" + (" " * $Padding) + "║")
+			Write-Host ("$offset" + "$fColor" + "╚" + ("═" * $innerWidth) + "$fColor" + "╝") -NoNewline:$NoNewLine
+		}
+
+		none {
+			Write-Host ($RawOffset + $Text) -NoNewline:$NoNewLine
+		}
+	}
 }
 
 $TitleIndent = [Math]::Max(0, [Math]::Floor(($conWidth - 48) / 2 - 1)) # -48 because thats how big the kon os logo is in columns
@@ -116,9 +146,9 @@ $offset2 ║                        $($esc)[97mBegin Setup?$accent              
 $offset2 ╚════════════════════════════════════════════════════════════╝
 
 $offset3 $($esc)[32m╭────────────╮$($esc)[31m          ╭────────────╮
-$offset3 $($esc)[32m│   ✅ [Y]   │$($esc)[31m          │   ❎ [N]   │
+$offset3 $($esc)[32m│   ☑️ [Y]   │$($esc)[31m          │   ✖️ [N]   │
 $offset3 $($esc)[32m╰────────────╯$($esc)[31m          ╰────────────╯
 $($LineOffset2)
 "@
-Write-Box -Text "$VerIndicator" -Border Round -Color Blue -NoNewLine
+Write-Box -Text "⚙️ $VerIndicator" -Border Round -Color Accent -HighlightText -Padding 2 -Indent 1 -NoNewLine 
 cmd /c "pause" | Out-Null
