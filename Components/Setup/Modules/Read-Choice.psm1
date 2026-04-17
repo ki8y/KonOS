f73f8529b07f5500fe376c3af57f1066a76fcfa3
Kon <199155976+ki8y@users.noreply.github.com>
2026-04-17 16:40:08 -0400

    Overhaul
    I wanted to make my own custom choice prompt instead of relying on
    choice.exe. Right now it's a total mess, but it absolutely works :D
    
    Known bugs:
    - If the little "»" prompt thingy is on the same line as the error
    screen, the "»" will breifly disappear before closing the error.
    - Completely breaks on conhost.exe (and possibly some other terminals
    with iffy esc sequence support)
    
    Planned changes:
    - Stabilize by not relying on ESC codes as much (if it's possible)
    - Fix error overwriting the "»" prompt

--- Components/Setup/Modules/Read-Choice.psm1
+++ Components/Setup/Modules/Read-Choice.psm1
@@ -1,16 +1,56 @@
 ﻿function Read-Choice {
     # helper function for YES/NO choices :P
 
-    Write-Host "» " -ForegroundColor Blue -NoNewLine
-    choice /c YN /n | Out-Null
-    switch ($LASTEXITCODE) {
-        1 { 
-            Write-Host "Yes"
-            return $true
-        }
-        2 {
-            Write-Host "No"
-            return $false
-        }
-    }
+    $esc = ([char]27)
+
+    $validResponse = $false
+
+    
+    $konSize = (Get-Host).UI.RawUI.WindowSize # Console size (konsize lololo)
+    
+    # Error thing
+    $ErrorMessage = "ⓧ Invalid key. Press any key to try again..."
+    $padding = 2
+    $InnerWidth = $ErrorMessage.Length + ($padding * 2)
+
+    $IndentX = [Math]::Max(0, [Math]::Floor(($konSize.Width - $ErrorMessage.Length) / 2 - 1))
+    $IndentY = [Math]::Max(0, [Math]::Floor(($konSize.Height - 3) / 2))
+
+    $OffsetX = " " * $IndentX
+    $OffsetY = "`n" * $IndentY
+
+    $fColor = "$($esc)[31m"
+    $highlighter = "$($esc)[97m"
+
+    do {
+        Write-Host "`r» " -ForegroundColor Blue -NoNewline; $cursorPos = (Get-Host).UI.RawUI.CursorPosition
+
+        switch (([System.Console]::ReadKey($true) | Select-Object -ExpandProperty Key)) {
+            Y {
+                Write-Host ("`r$($esc)[92m» " + "$($esc)[97m" + "Yes")
+                return $true
+                $validResponse = $true
+            }
+            N {
+                Write-Host ("`r$($esc)[91m» " + "$($esc)[97m" + "No")
+                return $false
+                $validResponse = $true
+            }
+            default {
+                # error message
+                Write-Host "$ESC[H" -NoNewline
+                
+                Write-Host ("$($OffsetY)`n$($offsetX)" + "$fColor" + "╭" + ("─" * $innerWidth) + "╮")
+                Write-Host ("$($offsetX)" + "$fColor" + "│" + (" " * $Padding) + "$Highlighter" + $ErrorMessage + "$fColor" + (" " * $Padding) + "│")
+                Write-Host ("$($offsetX)" + "$fColor" + "╰" + ("─" * $innerWidth) + "╯") -NoNewline
+                
+                [System.Console]::ReadKey($true) | Out-Null
+                Write-Host "$esc[3A"
+                $clearError = " " * ($innerWidth + 2)
+                Write-Host "$offsetx$clearError`n$offsetx$clearError`n$offsetx$clearError"
+                Write-Host "$esc[$($cursorPos.Y);$($cursorPos.X)f"
+                Write-Host "$esc[2C" -NoNewline
+            }
+        } 
+    } while (-not $validResponse)
 }
