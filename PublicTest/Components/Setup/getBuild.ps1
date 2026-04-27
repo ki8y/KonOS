10006d26fb83eabdebb21e34f1dfb4a4ba012566
Kon <199155976+ki8y@users.noreply.github.com>
2026-04-26 23:59:04 -0400

    lazy
    Added:
    - BypassVersionCheck Parameter
        - Skips check to see if your build is supported. Will still log your
    build in flags (once that's added)
    
    Changed:
    - Replaced Out-String with Write-Output for backwards compatibility with
    PS 5.1
    
    Planned changes:
    - Log build to flags
    - Every planned change from last commit

--- PublicTest/Components/Setup/getBuild.ps1
+++ PublicTest/Components/Setup/getBuild.ps1
@@ -1,31 +1,33 @@
 param (
-    [switch]$Test
+    [switch]$BypassVersionCheck
 )
 
 $buildList = Invoke-RestMethod -Uri "https://raw.githubusercontent.com/ki8y/KonOS/master/Components/Setup/Data/SupportedBuilds.json"
-$BuildID = [System.Environment]::OSVersion.Version.Build | Out-String -NoNewline
+$BuildID = [System.Environment]::OSVersion.Version.Build | Write-Output
 
 $Build = $buildList | Select-Object -ExpandProperty $BuildID
 
-switch ($Build.Support) {
-    Full {
-        Write-Host "Your version of Windows is supported!"
-    }
-    Limited {
-        Write-Host "Your version of Windows has limited support."
-    }
-    None {
-        Write-Host "FUCK YOUUUU"
-    }
-    Experimental {
-        Write-Host "Your version of Windows is in experimental testing."
-    }
-    default {
-        Write-Host "Your version of Windows is not whitelisted."
+if ($BypassVersionCheck) {
+    Write-Host "Skipping version check..." # might use this to bypass windows build detection later if the user really really wants to.
+}
+else {
+    switch ($Build.Support) {
+        Full {
+            
+        }
+        Limited {
+            Write-Host "Your version of Windows has limited support."
+        }
+        None {
+            Write-Host "Your version of Windows is not supported."
+        }
+        Experimental {
+            Write-Host "Your version of Windows is in experimental testing."
+        }
+        default {
+            Write-Host "Your version of Windows is not whitelisted."
+        }
     }
 }
-# [System.Console]::ReadKey($true) | Out-Null
 
-if ($Test) {
-    Write-Host "Test is active!" # might use this to bypass windows build detection later if the user really really wants to.
-}
+[System.Console]::ReadKey($true) | Out-Null
