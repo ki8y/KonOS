369589a1bd962c0827550ae9580931f3fa9e6431
Kon <199155976+ki8y@users.noreply.github.com>
2026-04-27 11:39:48 -0400

    Update getBuild.ps1
    Added:
    - Actual functionality!? (Still unfinished LOL)
    
    Planned changes:
    - No new planned changes, still need to get that other stuff done first.

--- PublicTest/Components/Setup/getBuild.ps1
+++ PublicTest/Components/Setup/getBuild.ps1
@@ -13,13 +13,25 @@ if ($BypassVersionCheck) {
 else {
     switch ($Build.Support) {
         Full {
-            
+            <# Do nothing lol #>
         }
         Limited {
-            Write-Host "Your version of Windows has limited support."
+            Write-Host @(
+                "Your version of Windows ($($Build.Name) $($Build.Version)) has limited support.`n",
+                "While compatible, I don't work hard to maintain support for these versions of Windows.",
+                "You may encounter bugs, errors, or other inconveniences. (If you do, report them please :D)`n",
+		"Press any key to proceed anyway..."
+            ) -Separator "`n" -ForegroundColor DarkYellow -NoNewline
+            [System.Console]::ReadKey($true) | Out-Null
         }
         None {
-            Write-Host "Your version of Windows is not supported."
+            Write-Host @(
+                "Your version of Windows ($($Build.Name) $($Build.Version)) is not supported.`n",
+                "I will not maintain support for these versions of Windows, and I'd highly recommend installing a newer version of Windows.",
+                "You may encounter bugs, errors, or other inconveniences.`n",
+		"Press any key to proceed anyway..."
+            ) -Separator "`n" -ForegroundColor Red -NoNewline
+            [System.Console]::ReadKey($true) | Out-Null
         }
         Experimental {
             Write-Host "Your version of Windows is in experimental testing."
