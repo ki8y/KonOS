e43964caebc7b18398701bdbd500d826b89ba8d5
Kon <199155976+ki8y@users.noreply.github.com>
2026-05-06 23:37:26 -0400

    A few changes
    Added:
    - Build check
      - If a tweak is made for a newer version of Windows you might not be
    using, it wont run.
    
    Changed:
    - Invoke-RestMethod actually points to the right link now lol

--- PublicTest/Components/Setup/RegistryTweaks.ps1
+++ PublicTest/Components/Setup/RegistryTweaks.ps1
@@ -1,14 +1,24 @@
-$Tweaks = Invoke-RestMethod -Uri 'https://This-Link-Doesnt-Exist-Yet.Lol'
+# Applies All Registry tweaks in RegistryTweaks.json
+$Tweaks = Invoke-RestMethod -Uri 'https://raw.githubusercontent.com/ki8y/KonOS/master/Components/Setup/Data/RegistryTweaks.json'
+$Flags = (Get-Content -Path "$env:systemDrive\Kon OS\Setup\flags.json" | ConvertFrom-Json) 
+
 foreach ($Category in $Tweaks.GeneralTweaks.PSObject.Properties) {
 
     foreach ($Tweak in $Category.Value.PSObject.Properties) {
 
-        $Jobs = $Tweak.Value
+        $Jobs = $Tweak.Value.Registry
 
         Write-Host "[i] Applying $($Tweak.Name) in registry..." -ForegroundColor Blue
-        foreach ($job in $jobs) {
-            Write-Host "$($Job.key), $($Job.Value), $($Job.Data), (Type):$($Job.Type)"
-            #[Microsoft.Win32.Registry]::SetValue($($Job.key), $($Job.Value), $($Job.Data), $($Job.Type))
+        
+        if ($Tweak.Value.MinBuild -gt $Flags.Build) {
+            Write-Host "  This tweak is made for a newer version of Windows, skipping..." -ForegroundColor Red
         }
+        else {
+            foreach ($job in $jobs) {
+                Write-Host "$($Job.key), $($Job.Value), $($Job.Data), (Type):$($Job.Type) $($Tweak.Value.MinBuild)"
+                #[Microsoft.Win32.Registry]::SetValue($($Job.key), $($Job.Value), $($Job.Data), $($Job.Type))
+            }
+        }
+
     }
 }
