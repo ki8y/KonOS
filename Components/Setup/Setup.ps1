ba100889aec63b761ed2433911d593ad667127a7
Kon <199155976+ki8y@users.noreply.github.com>
2026-07-08 02:09:17 -0400

    Small changes
    Added:
    - A parameter to skip the title screen, this way I dont need to turn the
    title screen into it's own separate script.
    
    Changed:
    - Version is no longer a hardcoded object, it now pulls from the github
    like it used to. Accidental debug thing I left in.
    - Path names are fixed to match the bootstrapper instead of my personal
    paths. Another debug thing.
    - The script used to pull from the master branch, not the rewrite
    branch. That's fixed now.

--- Components/Setup/Setup.ps1
+++ Components/Setup/Setup.ps1
@@ -1,31 +1,33 @@
-﻿$col = Get-Content "C:\Users\Wybie\Documents\GitHub\KonOSRedone\Components\Universal\Data\Colours.json" | ConvertFrom-Json
-Import-Module "C:\Users\Wybie\Documents\GitHub\KonOSRedone\Components\Universal\Modules\Write-Box.psm1"
+﻿param(
+    [switch]$SkipTitleScreen
+)
 
+Import-Module "$env:systemDrive\KonOS\Setup\Data\Variables.psm1"
+
+# Get colours
+$col = Get-Content "$KonOS\KonOS\Setup\Data\Colours.json" -Raw | ConvertFrom-Json
+Import-Module "$KonOS\Setup\Modules\Write-Box.psm1"
 
 # Pull Kon OS version :P
-$Commit = Invoke-RestMethod -Uri "https://api.github.com/repos/ki8y/KonOS/commits/master"
-$Ver = [PSCustomObject]@{
-    Major           = 1
-    Minor           = 1
-    Patch           = 0
-    PreReleaseLabel = "Development Hell"
-}
+$Commit = Invoke-RestMethod -Uri "https://api.github.com/repos/ki8y/KonOS/commits/KonOS-Rewrite"
+$Ver = Invoke-RestMethod -Uri "https://github.com/ki8y/KonOS/raw/KonOS-Rewrite/version.json"
 $VerString = "$($Ver.Major).$($Ver.Minor).$($Ver.Patch) $($Ver.PreReleaseLabel) ($($commit.sha.Substring(0,7)))"
 
-
-# Kon OS Title Screen.
 [Console]::OutputEncoding = [System.Text.Encoding]::UTF8 # enable utf8 stuff
-function Write-Title {
-    $ConsoleSize = $Host.UI.RawUI.WindowSize
 
-    $X = [Math]::Max(0, [Math]::Floor(($consoleSize.Width - 62) / 2))
-    $Y = [Math]::Max(0, [Math]::Floor(($consoleSize.Height - 6) / 2 - 3))
-    $Y2 = [Math]::Max(0, [Math]::Floor(($consoleSize.Height - $Y) - 14) - 4)
-    $XOffset = " " * $X
-    $YOffset = "`n" * $Y
-    $YOffset2 = "`n" * $Y2
+if (-not $SkipTitleScreen) {
+    # Kon OS Title Screen.
+    function Write-Title {
+        $ConsoleSize = $Host.UI.RawUI.WindowSize
 
-    $Title = @"
+        $X = [Math]::Max(0, [Math]::Floor(($consoleSize.Width - 62) / 2))
+        $Y = [Math]::Max(0, [Math]::Floor(($consoleSize.Height - 6) / 2 - 3))
+        $Y2 = [Math]::Max(0, [Math]::Floor(($consoleSize.Height - $Y) - 14) - 4)
+        $XOffset = " " * $X
+        $YOffset = "`n" * $Y
+        $YOffset2 = "`n" * $Y2
+
+        $Title = @"
 $($YOffset)$($col.Text.Accent)
 $XOffset       ██╗  ██╗ ██████╗ ███╗   ██╗     ██████╗ ███████╗
 $XOffset       ██║ ██╔╝██╔═══██╗████╗  ██║    ██╔═══██╗██╔════╝
@@ -43,44 +45,45 @@ $XOffset            $($col.Text.Green)│   ☑️ [Y]   │          $($col.Tex
 $XOffset            $($col.Text.Green)╰────────────╯          $($col.Text.Red)╰────────────╯$($Col.Text.White)
 $($YOffset2)
 "@
-    [Console]::Write($Title)
-    Write-Box -Text "⚙️ $VerString" -Border Round -Color Accent -HighlightText -Padding 2 -Indent 0 -NoNewLine
-}
-
-# Initialize title screen :D
-[Console]::CursorVisible = $false
-Clear-Host
-$bootSnd.Play()
-:TitleLoop while ($true) {
-    $ConsoleSize = $Host.UI.RawUI.WindowSize
-    $X = [Math]::Max(0, [Math]::Floor(($consoleSize.Width - 33) / 2 - 1))
-    $Y = [Math]::Max(0, [Math]::Floor(($consoleSize.Height - 1) / 2 - 1))
-    $XOffset = " " * $X
-    $YOffset = "`n" * $Y
-
-    if ($ConsoleSize.Width -ne $oldSize.Width -or $ConsoleSize.Height -ne $oldSize.Height) {
-
-        [Console]::Clear()
-        Write-Title
-        $oldSize = $ConsoleSize
-    
+        [Console]::Write($Title)
+        Write-Box -Text "⚙️ $VerString" -Border Round -Color Accent -HighlightText -Padding 2 -Indent 0 -NoNewLine
     }
-    if ([Console]::KeyAvailable) {
-        $Key = [Console]::ReadKey($true)
 
-        switch ($Key.Key) {
+    # Initialize title screen :D
+    [Console]::CursorVisible = $false
+    Clear-Host
+    $bootSnd.Play()
+    :TitleLoop while ($true) {
+        $ConsoleSize = $Host.UI.RawUI.WindowSize
+        $X = [Math]::Max(0, [Math]::Floor(($consoleSize.Width - 33) / 2 - 1))
+        $Y = [Math]::Max(0, [Math]::Floor(($consoleSize.Height - 1) / 2 - 1))
+        $XOffset = " " * $X
+        $YOffset = "`n" * $Y
 
-            'Y' {
-                break TitleLoop
-            }
+        if ($ConsoleSize.Width -ne $oldSize.Width -or $ConsoleSize.Height -ne $oldSize.Height) {
 
-            'N' {
-                break TitleLoop
+            [Console]::Clear()
+            Write-Title
+            $oldSize = $ConsoleSize
+    
+        }
+        if ([Console]::KeyAvailable) {
+            $Key = [Console]::ReadKey($true)
+
+            switch ($Key.Key) {
+
+                'Y' {
+                    break TitleLoop
+                }
+
+                'N' {
+                    break TitleLoop
+                }
             }
         }
-    }
 
-    Start-Sleep -Milliseconds 16
+        Start-Sleep -Milliseconds 16
+    }
 }
 Write-Host "`nTitle loop broken!"
 [Console]::CursorVisible = $true
